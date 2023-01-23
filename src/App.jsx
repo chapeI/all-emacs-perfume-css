bceb7698662deb923dad6b0cec98a7d8cf626c03
anoop <anoop.anthony@gmail.com>
2023-01-22 20:27:57 -0500

    trick for centering
    using grid (height: 100vh, grid, place-content: center)

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,27 @@ import img_desktop from "./images/image-product-desktop.jpg";
 export function App() {
   return (
     <body>
-      <article className="product">
-        <picture className="product__image">
-          <source srcSet={img_desktop} media="(min-width: 600px)" />
-          <img src={img} />
-        </picture>
-
-        <div className="product__text">
-          <div className="product__title">Gaby Eau de Parfum</div>
-          <div className="product__content">
-            Enter the world of CHANEL and discover the latest in Fashion &
-            Accessories, Eyewear, Fragrance, Skincare & Makeup, Fine Jewelry &
-            Watches.
-          </div>
-          <div className="flex">
-            <div className="product__price">$119.99</div>
-            <s>$149.99</s>
-          </div>
-          <button className="btn">Add</button>
-        </div>
-      </article>
+      <div className="center-in-middle-of-page">
+		<article className="product">
+		  <picture className="product__image">
+			<source srcSet={img_desktop} media="(min-width: 600px)" />
+			<img src={img} />
+		  </picture>
+		  <div className="product__text">
+			<div className="product__title">Gaby Eau de Parfum</div>
+			<div className="product__content">
+			  Enter the world of CHANEL and discover the latest in Fashion &
+			  Accessories, Eyewear, Fragrance, Skincare & Makeup, Fine Jewelry &
+			  Watches.
+			</div>
+			<div className="flex">
+			  <div className="product__price">$119.99</div>
+			  <s>$149.99</s>
+			</div>
+			<button className="btn">Add</button>
+		  </div>
+		</article>
+	  </div>
     </body>
   );
 }
