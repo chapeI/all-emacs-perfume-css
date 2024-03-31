91c47470220e2ba4c6921b9a26188d6dddb5f10e
anoop <anoop.anthony@gmail.com>
2024-03-31 18:32:06 -0400

    refactoring, better css classNames

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,12 @@
-import img from "./images/image-product-mobile.jpg";
-import img_desktop from "./images/image-product-desktop.jpg";
+import { Product } from "./Product.jsx";
 
 export function App() {
+
   return (
     <body>
-      <div className="center-in-middle-of-page">
-		<article className="product">
-		  <picture className="product__image">
-			<source srcSet={img_desktop} media="(min-width: 600px)" />
-			<img src={img} />
-		  </picture>
-		  <div className="product__text">
-			<div className="product__title">Gaby Eau de Parfum</div>
-			<div className="product__content">
-			  Enter the world of CHANEL and discover the latest in Fashion &
-			  Accessories, Eyewear, Fragrance, Skincare & Makeup, Fine Jewelry &
-			  Watches.
-			</div>
-			<div className="flex">
-			  <div className="product__price">$119.99</div>
-			  <s>$149.99</s>
-			</div>
-			<button className="btn">Add</button>
-		  </div>
-		</article>
-	  </div>
+      <div className="center-of-webpage">
+	  <Product />
+      </div>
     </body>
   );
 }
